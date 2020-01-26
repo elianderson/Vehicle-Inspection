a5ef618e2b21122819fe512e33285ff0c7ce2077
JP DeVries <mail@devries.jp>
2020-01-25 21:16:10 -0800

    chore: wip

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,5 @@
 module.exports = function(api) {
+
   var validEnv = ['development', 'test', 'production']
   var currentEnv = api.env()
   var isDevelopmentEnv = api.env('development')
@@ -37,6 +38,7 @@ module.exports = function(api) {
       ]
     ].filter(Boolean),
     plugins: [
+      '@babel/plugin-transform-react-jsx',
       'babel-plugin-macros',
       '@babel/plugin-syntax-dynamic-import',
       isTestEnv && 'babel-plugin-dynamic-import-node',

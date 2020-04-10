85b503c09eb775c8c09ed73ed6dc81a4dee93606
Eli Anderson <eli@theprogrampdx.com>
2020-04-10 08:29:04 -0700

    adding some gems

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root to: 'pages#index'
   scope :api do
     resources :inspections
+    resources :users
+    post '/login', to: 'auth#login'
+    post '/auto_login', to: 'auth#auto_login'
+    post '/secured', to: 'auth#secured'
   end
 
  get '*path', to: 'pages#index'
